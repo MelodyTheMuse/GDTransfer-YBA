51e0082b44874bfbdb1fd700acf1d71c2dd2994b
MelodyTheMuse <keanodussel@gmail.com>
2025-11-12 12:43:27 +0100

    Sequenser changed to 1 timer
    Changed the way that the sequenser works to a single timer instead of 4

--- Scripts/sequenzer.gd
+++ Scripts/sequenzer.gd
@@ -1,5 +1,7 @@
 extends Node
 
+class_name sequenzer
+
 @export_group("Timing")
 @export var bpm:float
 @export var notes_per_beat:int
@@ -23,11 +25,7 @@ const fixed_seconds:int = 60
 var seconds:float
 var _total_time :=0.0
 
-
-var kick_ring_timer:Timer = Timer.new()
-var klap_ring_timer:Timer = Timer.new()
-var trompet_ring_timer:Timer = Timer.new()
-var hihat_ring_timer:Timer = Timer.new()
+var ring_timer:Timer = Timer.new()
 var kick_ring_string:String = "kick_ring"
 var klap_ring_string:String = "klap_ring"
 var trompet_ring_string:String = "trompet_ring"
@@ -42,6 +40,32 @@ var trompet_ring:Array[bool]
 var hihat_ring:Array[bool]
 
 signal change_note_active_status(beat_ring_enum:beat_ring_button_resource.ring_types, note:int)
+
+func _ready() -> void:
+	name = "sequenser"
+	change_note_active_status.connect(_on_change_note_active_status)
+	_setup_array_sizes()
+	_calc_beat_and_note_length()
+	_players_setup()
+	_setup_dict()
+	_setup_timer(ring_timer)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("main_click"):
+		playing = !playing
+		ring_timer.autostart = !ring_timer.autostart
+		ring_timer.paused = !ring_timer.paused
+		if ring_timer.is_stopped():
+			ring_timer.start(0)
+	if !playing: 
+		_total_time = 0
+		return
+	_total_time += delta
+	seconds = fmod(_total_time,fixed_seconds)
+	if(notes[klap_ring_string] == notes_per_beat):
+		_beat +=1
+	if(_beat == beats_per_section):
+		_reset_counters()
 
 func _on_change_note_active_status(beat_ring_enum:beat_ring_button_resource.ring_types, note:int):
 	match beat_ring_enum:
@@ -64,7 +88,7 @@ func _reset_dict():
 	for c in notes:
 		notes.set(c,0) 
 
-func _calc_beat_time():
+func _calc_beat_and_note_length():
 	beat_length=fixed_seconds/bpm
 	print("beat_length ", beat_length)
 	note_length = beat_length / beats_per_section
@@ -88,80 +112,46 @@ func _players_setup():
 	add_child(trompet_player)
 	add_child(hihat_player)
 
-func _ready() -> void:
-	change_note_active_status.connect(_on_change_note_active_status)
-	_setup_array_size()
-	_calc_beat_time()
-	_players_setup()
-	_setup_dict()
-	_setup_timer(klap_ring_timer)
-	_setup_timer(kick_ring_timer)
-	_setup_timer(trompet_ring_timer)
-	_setup_timer(hihat_ring_timer)
-
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("main_click"):
-		#TODO change this to not be this big
-		playing = true
-		klap_ring_timer.autostart = true
-		klap_ring_timer.start(0)
-		kick_ring_timer.autostart = true
-		kick_ring_timer.start(0)
-		trompet_ring_timer.autostart = true
-		trompet_ring_timer.start(0)
-		hihat_ring_timer.autostart = true
-		hihat_ring_timer.start(0)
-	if !playing: 
-		_total_time = 0
-		return
-	_total_time += delta
-	seconds = fmod(_total_time,fixed_seconds)
-	if(notes[klap_ring_string] == notes_per_beat):
-		_beat +=1
-	if(_beat == beats_per_section):
-		_reset_state()
-
-func _play_sound_per_beat(timer):
+func _on_play(ring_array:Array, ring_string:String, ring_player:AudioStreamPlayer):
 	var i:int
-	match timer:
-		klap_ring_timer: 
-			if allowed_to_play(klap_ring,klap_ring_string):
-				klap_player.play(0)
-			i = notes[klap_ring_string]
-			i+=1
-			notes.set(klap_ring_string,i)
-		kick_ring_timer:
-			if allowed_to_play(kick_ring,kick_ring_string):
-				kick_player.play(0)
-			i = notes[kick_ring_string]
-			i+=1
-			notes.set(kick_ring_string,i)
-		trompet_ring_timer:
-			if allowed_to_play(trompet_ring,trompet_ring_string):
-				trompet_player.play(0)
-			i = notes[trompet_ring_string]
-			i+=1
-			notes.set(trompet_ring_string,i)
-		hihat_ring_timer:	
-			if allowed_to_play(hihat_ring,hihat_ring_string):
-				hihat_player.play(0)
-			i = notes[hihat_ring_string]
-			i+=1
-			notes.set(hihat_ring_string,i)
+	if _check_allowed_play_on_dict_value(ring_array,ring_string):
+		ring_player.play(0)
+	i = notes[ring_string]
+	i+=1
+	notes.set(ring_string,i)
 
+func _emit_on_play(ring_array:Array, ring_string:String,ring_type:beat_ring_button_resource.ring_types):
+	var i:int
+	if _check_allowed_play_on_dict_value(ring_array,ring_string):
+		pass
+	i = notes[ring_string]
+	i+=1
+	notes.set(ring_string,i)
 
-func _reset_state():
+func _on_timeout():
+	for k in notes.keys():
+		match k:
+			klap_ring_string: 
+				_on_play(klap_ring,klap_ring_string,klap_player)
+			kick_ring_string:
+				_on_play(kick_ring,kick_ring_string,kick_player)
+			trompet_ring_string:
+				_on_play(trompet_ring,trompet_ring_string,trompet_player)
+			hihat_ring_string:	
+				_on_play(hihat_ring,hihat_ring_string,hihat_player)
+
+func _reset_counters():
 	_total_time = 0
 	_beat = 0
 	_reset_dict()
 	print("one loop complete")
 
-func allowed_to_play(array:Array[bool],string:String) -> bool:
+func _check_allowed_play_on_dict_value(array:Array[bool],string:String) -> bool:
 	var i = notes[string]
 	var ring_bool = array.get(i)
 	return ring_bool
 
-func _setup_array_size():
+func _setup_array_sizes():
 	klap_ring.resize(notes_per_beat)
 	kick_ring.resize(notes_per_beat)
 	trompet_ring.resize(notes_per_beat)
@@ -171,5 +161,6 @@ func _setup_timer(timer:Timer):
 	#TODO Add swing
 	timer.wait_time = note_length 
 	timer.autostart= false
-	timer.timeout.connect(_play_sound_per_beat.bind(timer))
+	timer.timeout.connect(_on_timeout.bind())
+	timer.paused = true
 	add_child(timer)
