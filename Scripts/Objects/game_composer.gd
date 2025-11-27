7630f1bbb203dd6c90b52ff2fb4f4dda4aafafef
MelodyTheMuse <keanodussel@gmail.com>
2025-11-27 09:16:19 +0100

    Little abstraction added

--- Scripts/Objects/game_composer.gd
+++ Scripts/Objects/game_composer.gd
@@ -10,9 +10,11 @@ signal start_game()
 signal set_sequenser(sequence)
 signal set_audio_composer(audiocomposer)
 signal use_back_up
-signal set_rec_synths(recording)
+#Audio signals
+signal set_rec_synth(recording)
 signal play_synth(_time)
 signal stop_synth()
+signal play_ring_type(ring_type:beat_ring_button_resource.ring_types)
 
 func _ready() -> void:
 	start_game.connect(_on_start_game)
